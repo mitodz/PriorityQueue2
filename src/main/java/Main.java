a8b9fd0de8eee6e57312d6a2139c21fe3682b202
mitodz <mitodzzz@gmail.com>
2019-04-25 17:06:02 +0300

    In the beginning

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,14 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt(); //количество процессоров
+        int m = scanner.nextInt(); //количество процессов
+        Heap h = new Heap();
+        for (int i = 0; i < n; i++) {
+            h.insert(scanner.nextInt());
+        }
     }
 }
