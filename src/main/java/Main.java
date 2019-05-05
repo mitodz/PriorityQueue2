cff205fa20b7fc0c3ff067f8cc52cb64aee5db7e
mitodz <mitodzzz@gmail.com>
2019-05-05 20:18:41 +0300

    Decided to use existing implementations of Priority Queue and Comparator objects

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+import javafx.scene.layout.Priority;
+
 import java.util.AbstractMap;
 import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Main {
@@ -8,9 +11,21 @@ public class Main {
         Scanner scanner = new Scanner("2 5 1 2 3 4 5");
         int n = scanner.nextInt(); //количество процессоров
         int m = scanner.nextInt(); //количество процессов
-        Comparator<int[]> c1 = (x, y) -> x[1] >= y[1] ? 1 : -1; //компаратор времени
-        Comparator<int[]> c2 = (x, y) -> x[0] <= y[0] ? 1 : -1; //компаратор нумерации процессора
-        Comparator<int[]> c3 = c1.thenComparing(c2); //финальный компаратор
+        long[][] a = new long[][][n][2];
+        Comparator<long[]> c1 = (x, y) -> x[1] <= y[1] ? 1 : -1; //компаратор времени
+        Comparator<long[]> c2 = (x, y) -> x[0] <= y[0] ? 1 : -1; //компаратор нумерации процессора
+        Comparator<long[]> c3 = c1.thenComparing(c2); //финальный компаратор
+        PriorityQueue<long[]> q = new PriorityQueue<>(c3);
+        for (int i = 0; i < n; i++) {
+            a[i][0] = i;
+            q.add(a[i]);
+            System.out.print(i + " 0\n");
+        }
+        for (int i = 0; i < m; i++) {
+
+        }
+
+
 
 
 
