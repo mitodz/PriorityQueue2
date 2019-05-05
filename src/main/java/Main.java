be9b7e9ef323466701d72f132a83f79fa4bd1e55
mitodz <mitodzzz@gmail.com>
2019-05-05 20:25:02 +0300

    Decided to use existing implementations of Priority Queue and Comparator objects

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,22 +11,22 @@ public class Main {
         Scanner scanner = new Scanner("2 5 1 2 3 4 5");
         int n = scanner.nextInt(); //количество процессоров
         int m = scanner.nextInt(); //количество процессов
-        long[][] a = new long[][][n][2];
-        Comparator<long[]> c1 = (x, y) -> x[1] <= y[1] ? 1 : -1; //компаратор времени
+        long[][] a = new long[n][2];
+        Comparator<long[]> c1 = (x, y) -> x[1] >= y[1] ? 1 : -1; //компаратор времени
         Comparator<long[]> c2 = (x, y) -> x[0] <= y[0] ? 1 : -1; //компаратор нумерации процессора
         Comparator<long[]> c3 = c1.thenComparing(c2); //финальный компаратор
         PriorityQueue<long[]> q = new PriorityQueue<>(c3);
         for (int i = 0; i < n; i++) {
             a[i][0] = i;
             q.add(a[i]);
-            System.out.print(i + " 0\n");
+            //System.out.print(i + " 0\n");
         }
         for (int i = 0; i < m; i++) {
-
+            long[] j = q.poll();
+            System.out.print(j[0] + " " + j[1] + "\n");
+            j[1] += scanner.nextInt();
+            q.add(j);
         }
-
-
-
 
 
     }
